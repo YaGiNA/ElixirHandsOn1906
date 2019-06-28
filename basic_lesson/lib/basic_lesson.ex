6c320c61cac5b828f4f02626e4f8e5c4c436c696
YaGiNA <yutachihaya@gmail.com>
2019-06-28 19:23:04 +0900

    Make new project by mix cmd

---
+++ basic_lesson/lib/basic_lesson.ex
@@ -0,0 +1,18 @@
+defmodule BasicLesson do
+  @moduledoc """
+  Documentation for BasicLesson.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BasicLesson.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
