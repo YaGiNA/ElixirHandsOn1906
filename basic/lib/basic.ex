dff96f376493592213c0e8c1820b2bdcf5c1c230
YaGiNA <yutachihaya@gmail.com>
2019-06-28 20:00:09 +0900

    Make script of q1

--- basic/lib/basic.ex
+++ basic/lib/basic.ex
@@ -15,4 +15,26 @@ defmodule Basic do
   def hello do
     :world
   end
+
+  def q1 do
+    # Reading file
+    data = File.stream!("data/data.json")
+
+    # Convert JSON to map
+    map_data = Enum.map(data, fn d -> Poison.decode!(d) end)
+
+    # Filter by under age: age <= 20
+    filtered_data = Enum.filter(map_data, fn d -> d["age"] <= 20 end)
+
+    # Get amount of filtered rows
+    Enum.count(filtered_data)
+  end
+
+  def q1-2 do
+    "data.json"
+    |> File.stream!
+    |> Enum.map(fn d -> Poison.decode!(d) end)
+    |> Enum.filter(fn d -> d["age"] <= 20 end)
+    |> Enum.count
+  end
 end
